d602d30f438bc88675048dba32cbba492343a39d
q8135434 <40625662+q8135434@users.noreply.github.com>
2024-02-20 09:49:59 +0800

    commit
    修复插件未导入EnemyCharacter节点

--- GdCharacterPlugin/addons/gdcharacterplugin/gdcharacterplugin.gd
+++ GdCharacterPlugin/addons/gdcharacterplugin/gdcharacterplugin.gd
@@ -6,6 +6,8 @@ func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	add_custom_type("Characters", "Node", preload("res://addons/gdcharacterplugin/Characters/Characters.gd"), EditorInterface.get_base_control().get_theme_icon("Node", "EditorIcons"))
 	add_custom_type("PlayerCharacter", "Node", preload("res://addons/gdcharacterplugin/Characters/PlayerCharacter.gd"), EditorInterface.get_base_control().get_theme_icon("Node", "EditorIcons"))
+	add_custom_type("EnemyCharacter", "Node", preload("res://addons/gdcharacterplugin/Characters/EnemyCharacter.gd"), EditorInterface.get_base_control().get_theme_icon("Node", "EditorIcons"))
+	
 	pass
 
 
@@ -13,4 +15,5 @@ func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
 	remove_custom_type("Characters")
 	remove_custom_type("PlayerCharacter")
+	remove_custom_type("EnemyCharacter")
 	pass
